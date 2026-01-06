b6ecc1ca2f599966ced3e593bb9670eaa9233757
Marrya-beep <marrya.ah.2004@gmail.com>
2026-01-06 11:07:54 +0330

    Adding Swagger Category

---
+++ final/Shop/Shop/Controllers/CategoryAPIController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Data;
+
+namespace Shop.Controllers
+{
+    [ApiController]
+    [Route("api/category")]
+    public class CategoryAPIController : Controller
+    {
+
+        private readonly ShopDbContext _context;
+
+        public CategoryAPIController(ShopDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("category-list")]
+        public IActionResult GetCategories()
+        {
+            var categories = _context.Categories
+                .Select(c => c.Name)
+                .ToList();
+
+            return Ok(categories);
+        }
+
+    }
+}
